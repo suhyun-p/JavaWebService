de570a82eb786c6da04bd2e9f2e1b0f0894d47c9
park suhyun <laztwo@hanmail.net>
2018-09-30 17:48:52 +0900

    Chapter 5. Rest

---
+++ src/main/java/com/example/demo/model/TodoResource.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import org.springframework.hateoas.ResourceSupport;
+
+public class TodoResource extends ResourceSupport {
+
+    private String title;
+
+    public TodoResource() {
+    }
+
+    public TodoResource(String title) {
+
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,67 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+
+import java.util.Date;
+
+@ApiModel
+public class User {
+    private Integer userNo;
+    private String userId;
+    private String email;
+    private String uname;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date regDate;
+
+    public User() {
+    }
+
+    public User(Integer userNo, String userId, String email, String uname, Date regDate) {
+        this.userNo = userNo;
+        this.userId = userId;
+        this.email = email;
+        this.uname = uname;
+        this.regDate = regDate;
+    }
+
+    public Integer getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(Integer userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+}
